a1f647abe5cfd1a78a178f47e037cae795f7e3d9
mehmet <mehmetkicirti@hotmail.com>
2019-05-23 00:46:52 +0300

    Using with entity framework provided to connect database and crud codes writed

---
+++ SomethingHere.DataAccess/Partials/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomethingHere.DataAccess
+    //partial kullanırken quantity üründe anlık ekleme yaparken kullanılır bu class calısırken kordineli calıscak ve partial class varsa bunlar birbirini ezebilir demektir aynı namespace ile aynı isim ile kullanılır
+{
+    public partial class Product
+    {
+        public int Quantity { get; set; }
+        public decimal TotalPrice {//geriye döndürmücek bundan dolayı kullanabliriz..
+            get
+            {
+                return (decimal)(this.Quantity * this.Price);
+            }
+                
+                }
+    }
+}
